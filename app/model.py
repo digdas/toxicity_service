32daefafc2853a9be7f3b8844b8a9b7ecf778fde
Stanislav Ulegin <stash.horoshaya@yandex.ru>
2024-11-07 21:30:43 +0300

    change: add logging

--- app/model.py
+++ app/model.py
@@ -1,4 +1,5 @@
 from transformers import pipeline
+from app.logger import logger  # Import the logger
 
 def load_model():
     return pipeline("text-classification", model="cointegrated/rubert-tiny-toxicity", tokenizer="cointegrated/rubert-tiny-toxicity")
@@ -6,6 +7,10 @@ def load_model():
 def predict_toxicity(model, text):
     # Predict the toxicity for the input text
     predictions = model(text)  # Assuming you have a method to predict toxicity
+
+    # Log the input text and prediction result
+    logger.info(f"Input text: {text}")
+    logger.info(f"Predictions: {predictions}")
 
     # Map the prediction results to labels and scores
     labels_with_scores = []
