a681877a4e49b0de0fc461f1073476b487f1a537
Stanislav Ulegin <stash.horoshaya@yandex.ru>
2024-11-08 11:44:03 +0300

    change: add all labels

--- app/model.py
+++ app/model.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 from app.logger import logger  # Import the logger
 
 def load_model():
-    return pipeline("text-classification", model="cointegrated/rubert-tiny-toxicity", tokenizer="cointegrated/rubert-tiny-toxicity")
+    return pipeline("text-classification", model="cointegrated/rubert-tiny-toxicity", tokenizer="cointegrated/rubert-tiny-toxicity", return_all_scores=True)
 
 def predict_toxicity(model, text):
     # Predict the toxicity for the input text
@@ -14,7 +14,7 @@ def predict_toxicity(model, text):
 
     # Map the prediction results to labels and scores
     labels_with_scores = []
-    for label, score in predictions:  # Assuming predictions return a label-score tuple
-        labels_with_scores.append({"label": label, "score": score})
+    for predict in predictions[0]:  # Assuming predictions return a label-score tuple
+        labels_with_scores.append({"label": predict['label'], "score": predict['score']})
 
     return labels_with_scores
